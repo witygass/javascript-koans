0b5d91255e94826624be17facb1fac60777eebf8
witygass <witygass@indiana.edu>
2016-10-24 15:58:44 -0400

    moved from old fork to new

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -1,9 +1,10 @@
-var _; // globals
+var _; //globals
 
 describe("About Applying What We Have Learnt", function() {
+
   var products;
 
-  beforeEach(function () { 
+  beforeEach(function () {
     products = [
        { name: "Sonoma", ingredients: ["artichoke", "sundried tomatoes", "mushrooms"], containsNuts: false },
        { name: "Pizza Primavera", ingredients: ["roma", "sundried tomatoes", "goats cheese", "rosemary"], containsNuts: false },
@@ -16,6 +17,7 @@ describe("About Applying What We Have Learnt", function() {
   /*********************************************************************************/
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (imperative)", function () {
+
     var i,j,hasMushrooms, productsICanEat = [];
 
     for (i = 0; i < products.length; i+=1) {
@@ -30,35 +32,48 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
+
       var productsICanEat = [];
+      var thosewonuts = [];
 
+      var notShrooms = function(x) { return x != 'mushrooms' };
       /* solve using filter() & all() / any() */
+      thosewonuts = _(products).filter(function(x){return x.containsNuts === false});
+      productsICanEat = _(thosewonuts).filter(function(x) {return _(x.ingredients).all(notShrooms)});
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (imperative)", function () {
-    var sum = 0;
 
+    var sum = 0;
     for(var i=1; i<1000; i+=1) {
       if (i % 3 === 0 || i % 5 === 0) {
         sum += i;
       }
     }
-    
-    expect(sum).toBe(FILL_ME_IN);
+
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
 
-    expect(233168).toBe(FILL_ME_IN);
+    var sum = 233168;    /* try chaining range() and reduce() */
+    var divsOfFiveAndThree = function(sum, el) {
+      if (el % 3 === 0 || el % 5 === 0 ){
+        return sum += el
+      }
+      else return sum
+    };
+    var mySum = (_.range(1,1000)).reduce(divsOfFiveAndThree, 0);
+
+    expect(233168).toBe(mySum);
   });
 
   /*********************************************************************************/
@@ -71,39 +86,24 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   it("should count the ingredient occurrence (functional)", function () {
     var ingredientCount = { "{ingredient name}": 0 };
+    var objIngredients = function(obj){return obj.ingredients;}
+    var addToCounts = function(ingredientTotal, currIngredient){
+      return ingredientCount[currIngredient] = (ingredientCount[currIngredient] || 0) + 1;
+    };
 
     /* chain() together map(), flatten() and reduce() */
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    _(products).chain()
+    .map(objIngredients)
+    .flatten()
+    .reduce(addToCounts);
+
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
-  /*********************************************************************************/
-  /* UNCOMMENT FOR EXTRA CREDIT */
-  /*
-  it("should find the largest prime factor of a composite number", function () {
-  
-  });
-
-  it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
-    
-  });
-
-  it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
-      
-    
-  });
-
-  it("should find the difference between the sum of the squares and the square of the sums", function () {
-    
-  });
-
-  it("should find the 10001st prime", function () {
-
-  });
-  */
 });
